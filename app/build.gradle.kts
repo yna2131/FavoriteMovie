a27fe101b62167d6f482365b7a7eda8ad49724f3
yna2131 <jn7924@gmail.com>
2023-10-10 11:43:14 -0600

    Add dependencies to bulid.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,28 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation ("jp.wasabeef:glide-transformations:4.3.0")
+
+    //Corrutinas
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+
+    //Fragment
+    implementation ("androidx.fragment:fragment-ktx:1.5.0")
+    //Activity
+    implementation ("androidx.activity:activity-ktx:1.5.0")
+    //Databinding
+    implementation ("androidx.databinding:databinding-runtime:7.1.2")
+
+    // ViewModel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    // LiveData
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
 }
